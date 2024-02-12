50b116c6a281ef37ba9a5b37543f71e85315b227
ohMySunset <int4bute@gmail.com>
2024-02-12 17:09:52 +0900

    test: 테스트커밋

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -32,7 +32,7 @@ class _LoginPage extends State<LoginPage> {
 
   _asyncMethod() async {
     // read 함수로 key값에 맞는 정보를 불러오고 데이터타입은 String 타입
-    // 데이터가 없을때는 null을 반환
+    // 데이터가 없을때는 null을 반환..
     userInfo = await storage.read(key: 'login');
     // user의 정보가 있다면 로그인 후 들어가는 첫 페이지로 넘어가게 합니다.
     if (userInfo != null) {
